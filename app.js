d9f175c3ffd3a05d85f7b267978620c6f8099848
Eric Dvorsak <eric@dvorsak.fr>
2018-01-29 16:43:26 +0100

    trying whisper in bot

--- app.js
+++
@@ -1,9 +0,0 @@
-function onContentLoaded() {
-
-
-    var accountSpan = document.getElementById("account");
-    accountSpan.innerHTML =  
-        (typeof web3 === "undefined" ? "undefined" : web3.eth.accounts);
-
-}
-document.addEventListener("DOMContentLoaded", onContentLoaded);
